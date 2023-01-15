3bf267b0889f8eb7378c6c584f317f91cc950737
sykx <sy.p.sxxxx@gmail.com>
2023-01-15 15:55:56 +0900

    はろわ2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = "Hello World"
-    return name
+    #return name
+
+    return render_template('main.html', title='😺社会性フィルター😺', name=name) #変更
 
 @app.route('/good')
 def good():
