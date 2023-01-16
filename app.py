7ec22284f52c22e9ea020bcb09b52a5bb8988e62
sykx <sy.p.sxxxx@gmail.com>
2023-01-16 15:49:29 +0900

    ネコチャンの画像を出せるようになったよ！!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 import time
 
 IMAGE_PATH = './templates/src'
@@ -7,17 +7,27 @@ app = Flask(__name__, static_folder=IMAGE_PATH)
 
 @app.route('/')
 def main():
+    print(url_for('filtering',num=1))
     return render_template('main.html')
 
-@app.route('/filtering', methods=['GET','POST'])
-def filtering():
+@app.route('/redirect', methods=['POST'])
+def redirection():
+    # POSTの内容は捨てる
+    # 各種ネコチャンのページへリダイレクト
+    num = int(time.time()) % CAT_TOTAL
+
+    return redirect(url_for('filtering',num=num))
+
+@app.route('/filtering/<num>')
+def filtering(num):
     # POSTの内容は捨てる
 
     # ネコチャンの画像はUNIX TIME基準で適当に
-    num = int(time.time()) % CAT_TOTAL
-    path = "src/img/cat/" + str(num) +".jpg"
+    cat_num = str(num)
+    path = "src/img/cat/" + cat_num +".jpg"
 
-    return render_template('filtering.html', cat_image=path, img_num=str(num))
+    return render_template('filtering.html', cat_image=path, img_num=cat_num)
+
 
 ## おまじない
 if __name__ == "__main__":
